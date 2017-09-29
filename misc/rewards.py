c12844f287d46df957189d21a09c97686d73cd84
pgt4861@gmail.com <pgt4861@gmail.com>
2017-09-29 12:36:14 +0900

    170930 start

---
+++ misc/rewards.py
@@ -0,0 +1,117 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import time
+import misc.utils as utils
+from collections import OrderedDict
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+import sys
+sys.path.append("cider")
+from pyciderevalcap.ciderD.ciderD import CiderD
+
+CiderD_scorer = CiderD(df='coco-train-idxs')
+#CiderD_scorer = CiderD(df='corpus')
+
+def array_to_str(arr):
+    out = ''
+    for i in range(len(arr)):
+        out += str(arr[i]) + ' '
+        if arr[i] == 0:
+            break
+    return out.strip()
+
+#def get_self_critical_reward(model, fc_feats, att_feats, data, gen_result):
+def get_self_critical_reward(model, fc_feats, data, gen_result):
+    batch_size = gen_result.size(0)# batch_size = sample_size * seq_per_img
+    seq_per_img = batch_size // len(data['gts'])
+    
+    # get greedy decoding baseline
+    #greedy_res, _ = model.sample(Variable(fc_feats.data, volatile=True), Variable(att_feats.data, volatile=True))
+    greedy_res, _ = model.sample(Variable(fc_feats.data, volatile=True))
+
+    res = OrderedDict()
+    
+    gen_result = gen_result.cpu().numpy()
+    greedy_res = greedy_res.cpu().numpy()
+    for i in range(batch_size):
+        res[i] = [array_to_str(gen_result[i])]
+    for i in range(batch_size):
+        res[batch_size + i] = [array_to_str(greedy_res[i])]
+
+    gts = OrderedDict()
+    for i in range(len(data['gts'])):
+        gts[i] = [array_to_str(data['gts'][i][j]) for j in range(len(data['gts'][i]))]
+
+    #_, scores = Bleu(4).compute_score(gts, res)
+    #scores = np.array(scores[3])
+    res = [{'image_id':i, 'caption': res[i]} for i in range(2 * batch_size)]
+    gts = {i: gts[i % batch_size // seq_per_img] for i in range(2 * batch_size)}
+    _, scores = CiderD_scorer.compute_score(gts, res)
+    print('Cider scores:', _)
+
+    scores = scores[:batch_size] - scores[batch_size:]
+
+    rewards = np.repeat(scores[:, np.newaxis], gen_result.shape[1], 1)
+
+    return rewards
+
+
+def get_gan_reward(model_G, model_D, criterion_D, fc_feats, data):
+    batch_size = fc_feats.size(0)  # batch_size = sample_size * seq_per_img
+    seq_per_img = batch_size // len(data['gts'])
+
+    # get greedy decoding baseline
+    sample_res, sample_logprobs = model_G.sample(Variable(fc_feats.data, volatile=True), {'sample_max': 0})  # 640, 16
+    greedy_res, greedy_logprobs = model_G.sample(Variable(fc_feats.data, volatile=True), {'sample_max': 1})  # 640, 16
+
+    sample_res_embed = model_G.embed(Variable(sample_res, volatile=True)).cuda()
+    greedy_res_embed = model_G.embed(Variable(greedy_res, volatile=True)).cuda()
+
+    f_label = Variable(torch.FloatTensor(data['fc_feats'].shape[0]).cuda())
+    f_label.data.fill_(0)
+
+    f_D_sample_output = model_D(sample_res_embed.detach(), fc_feats.detach())
+    f_D_greedy_output = model_D(greedy_res_embed.detach(), fc_feats.detach())
+
+    # f_sample_loss = criterion_D(f_D_sample_output, f_label.long())
+    # f_greedy_loss = criterion_D(f_D_greedy_output, f_label.long())
+    # scores = f_sample_loss - f_greedy_loss
+    # scores = scores.data.cpu().numpy()
+
+    f_sample_loss = LossForEachBatch(f_D_sample_output, f_label, mode='NLL')
+    f_greedy_loss = LossForEachBatch(f_D_greedy_output, f_label, mode='NLL')
+    scores = f_sample_loss + f_greedy_loss
+    #print('GAN scores: %f' % scores)
+
+    rewards = np.repeat(scores[:, np.newaxis], sample_res.size(1), 1)
+
+    return rewards
+
+
+def LossForEachBatch(outputs, labels, mode):
+    if mode == 'BCE':
+        # output must have passed through F.SIGMOID
+        # loss = [-(torch.log(outputs[i]) * (labels[i]) + torch.log(1 - outputs[i]) * (1 - labels[i]))
+        #         for i in range(outputs.size(0))]
+        # return loss
+        pass
+    elif mode == 'NLL':
+        outputs = F.softmax(outputs)
+        #labels = Variable(torch.FloatTensor(labels.data.cpu().numpy())).cuda()
+        labels = labels.cuda()
+
+        loss = [-(torch.log(outputs[i][1]) * labels[i] + torch.log(outputs[i][0]) * (1 - labels[i])).data.cpu().numpy()[0]
+                for i in range(outputs.size(0))]
+        return np.array(loss)
+
+    elif mode == 'Acc':
+        # loss = [outputs[i][1] for i in range(outputs.size(0))]
+        # return loss
+        pass
+    else:
+        raise Exception('mode options must be BCE or NLL.')
---
+++ train_discriminator.py
@@ -0,0 +1,141 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import *
+import misc.utils as utils
+from torch import optim
+import time
+from six.moves import cPickle
+from models.ShowTellModel import ShowTellModel, Discriminator
+
+
+class Discriminator_trainer():
+
+    def __init__(self, opt):
+        #super(Discriminator_trainer, self).__init__(opt)
+
+        self.opt = opt
+        self.model_G = ShowTellModel(self.opt)
+        self.load_pretrain_model_G()
+        self.init_opt()
+        self.model_D = Discriminator(self.opt)
+
+        self.data_loader = None
+        self.max_length = 16
+
+        self.criterion_D = nn.CrossEntropyLoss(size_average=True)
+
+        """ only update trainable parameters """
+        D_parameters = filter(lambda p: p.requires_grad, self.model_D.parameters())
+        self.D_optimizer = optim.Adam(D_parameters, lr=self.opt.learning_rate * 1e-1)
+        self.iteration = 0
+
+    def load_pretrain_model_G(self):
+        self.model_G.load_state_dict(torch.load('save/model.pth'))
+
+    def init_opt(self):
+        self.opt.max_epoch = 10
+        self.opt.save_checkpoint_every = 1000
+        self.opt.start_from = None
+
+    def train_discriminator(self, dataloader):
+
+        for group in self.D_optimizer.param_groups:
+            group['lr'] = 0.0001
+
+        self.model_D.cuda()
+        self.model_G.cuda()
+
+        while True:
+            self.iteration += 1
+            data = dataloader.get_batch('train')
+
+            torch.cuda.synchronize()
+
+            tmp = [data['fc_feats'], data['labels'], data['masks']]
+            tmp = [Variable(torch.from_numpy(_), requires_grad=False).cuda() for _ in tmp]
+
+            fc_feats, labels, masks = tmp
+
+            self.model_D.zero_grad()
+            self.D_optimizer.zero_grad()
+
+            fc_feats = Variable(fc_feats.data.cpu(), volatile=True).cuda()
+            labels = Variable(labels.data.cpu()).cuda()
+
+            sample_res, sample_logprobs = self.model_G.sample(fc_feats, {'sample_max':0}) #640, 16
+            greedy_res, greedy_logprobs = self.model_G.sample(fc_feats, {'sample_max':1}) #640, 16
+            gt_res = labels # 640, 18
+
+            sample_res_embed = self.model_G.embed(Variable(sample_res))
+            greedy_res_embed = self.model_G.embed(Variable(greedy_res))
+            gt_res_embed = self.model_G.embed(gt_res)
+
+            f_label = Variable(torch.FloatTensor(data['fc_feats'].shape[0]).cuda())
+            r_label = Variable(torch.FloatTensor(data['fc_feats'].shape[0]).cuda())
+            f_label.data.fill_(0)
+            r_label.data.fill_(1)
+
+            f_D_output = self.model_D(sample_res_embed.detach(), fc_feats.detach())
+            f_loss = self.criterion_D(f_D_output, f_label.long())
+            f_loss.backward()
+
+            r_D_output = self.model_D(gt_res_embed.detach(), fc_feats.detach())
+            r_loss = self.criterion_D(r_D_output, r_label.long())
+            r_loss.backward()
+
+            D_loss = f_loss + r_loss
+            self.D_optimizer.step()
+
+            torch.cuda.synchronize()
+
+            if self.iteration % 1 == 0:
+                print('[%d/%d] Discriminator loss : %f' %(self.iteration, len(dataloader)/128, D_loss.data.cpu().numpy()[0]))
+
+            # make evaluation on validation set, and save model
+            #if (self.iteration % self.opt.save_checkpoint_every == 0):
+            if (self.iteration % 20 == 0):
+
+                checkpoint_path = os.path.join(self.opt.checkpoint_path, 'model_D.pth')
+                torch.save(self.model_D.state_dict(), checkpoint_path)
+                print("model saved to {}".format(checkpoint_path))
+                optimizer_path = os.path.join(self.opt.checkpoint_path, 'optimizer_D.pth')
+                torch.save(self.D_optimizer.state_dict(), optimizer_path)
+
+                best_flag = 1
+                if best_flag:
+                    checkpoint_path = os.path.join(self.opt.checkpoint_path, 'model_D-best.pth')
+                    torch.save(self.model_D.state_dict(), checkpoint_path)
+                    print("best model saved to {}".format(checkpoint_path))
+
+
+    def valid_discriminator(self):
+        self.save_discriminator()
+
+    def save_discriminator(self):
+        checkpoint_path = os.path.join(self.opt.expr_dir, 'model_D_pretrained.pth')
+        torch.save(self.model_D.state_dict(), checkpoint_path)
+
+        print("model saved to {}".format(checkpoint_path))
+        optimizer_path = os.path.join(self.opt.expr_dir, 'optimizer_D_pretrained.pth')
+        torch.save(self.D_optimizer.state_dict(), optimizer_path)
+
+
+
+if __name__ == "__main__":
+    import opts
+    from dataloader import *
+
+    opt = opts.parse_opt()
+    opt.use_att = False
+    loader = DataLoader(opt)
+    opt.vocab_size = loader.vocab_size
+    opt.seq_length = loader.seq_length
+
+    trainer = Discriminator_trainer(opt)
+    trainer.train_discriminator(loader)
